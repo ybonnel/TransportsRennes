bb0447565cf7cf232a8ea2afbd2d7fc91681c73c
ybonnel <ybonnel@gmail.com>
2010-12-14 16:33:45 +0000

    Ajout du layout inspiré de google IO scheduler afin d'avoir une page d'accueil propre.

---
+++ src/fr/ybo/transportsrennes/TransportsRennes.java
@@ -0,0 +1,20 @@
+package fr.ybo.transportsrennes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ybonnel
+ * Date: 14/12/10
+ * Time: 16:26
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransportsRennes extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+		setContentView(R.layout.main);
+    }
+}
