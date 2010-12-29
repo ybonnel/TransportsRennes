75a53ed5d14e713c54adeb3ee54e45df71082012
ybonnel <ybonnel@gmail.com>
2010-12-29 15:27:04 +0100

    Modification de la gestion de ArretRoute.

--- TransprotsRennesHelper/src/fr/ybo/transportsrenneshelper/keolis/gtfs/modele/ArretRoute.java
+++ TransprotsRennesHelper/src/fr/ybo/transportsrenneshelper/keolis/gtfs/modele/ArretRoute.java
@@ -1,10 +1,17 @@
 package fr.ybo.transportsrenneshelper.keolis.gtfs.modele;
 
+import fr.ybo.transportsrenneshelper.keolis.gtfs.annotation.BaliseCsv;
+import fr.ybo.transportsrenneshelper.keolis.gtfs.annotation.FichierCsv;
+
+@FichierCsv("arret_route.txt")
 public class ArretRoute {
+	@BaliseCsv("stop_id")
 	private String arretId;
 
+	@BaliseCsv("stop_id")
 	private String routeId;
 
+	@BaliseCsv("direction")
 	private String direction;
 
 	public String getArretId() {
