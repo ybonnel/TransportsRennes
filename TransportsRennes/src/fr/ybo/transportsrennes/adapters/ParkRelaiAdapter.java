90a070e281ee60531907ddbc9b2af345546c1e24
ybonnel <ybonnel@gmail.com>
2011-01-09 15:23:54 +0100

    Gestion  de l'état du park relai.

--- TransportsRennes/src/fr/ybo/transportsrennes/adapters/ParkRelaiAdapter.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/adapters/ParkRelaiAdapter.java
@@ -12,7 +12,9 @@ import fr.ybo.transportsrennes.keolis.modele.bus.ParkRelai;
 import fr.ybo.transportsrennes.keolis.modele.velos.Station;
 import fr.ybo.transportsrennes.util.Formatteur;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Adapteur pour les park relais.
@@ -29,6 +31,13 @@ public class ParkRelaiAdapter extends ArrayAdapter<ParkRelai> {
 
 	private static final double SEUIL_ORANGE = 0.5;
 
+	private static final Map<Integer, String> MAP_STATES = new HashMap<Integer, String>();
+	static {
+		MAP_STATES.put(1, "Fermé");
+		MAP_STATES.put(2, "Complet");
+		MAP_STATES.put(3, "Indispo.");
+	}
+
 	public ParkRelaiAdapter(Context context, int textViewResourceId, List<ParkRelai> objects) {
 		super(context, textViewResourceId, objects);
 		parkRelais = objects;
@@ -39,23 +48,31 @@ public class ParkRelaiAdapter extends ArrayAdapter<ParkRelai> {
 		LayoutInflater vi = LayoutInflater.from(getContext());
 		View v = vi.inflate(R.layout.dispoparkrelai, null);
 		ParkRelai parkRelai = parkRelais.get(position);
-		double poucentageDispo = ((double) parkRelai.getCarParkAvailable().intValue()) / ((double) (parkRelai.getCarParkCapacity().intValue()));
-
-		ImageView icone = (ImageView) v.findViewById(R.id.dispoparkrelai_image);
-		if (poucentageDispo < SEUIL_ROUGE) {
-			icone.setImageResource(R.drawable.dispo_parkrelai_rouge);
-		} else if (poucentageDispo < SEUIL_ORANGE) {
-			icone.setImageResource(R.drawable.dispo_parkrelai_orange);
-		} else {
-			icone.setImageResource(R.drawable.dispo_parkrelai_bleue);
-		}
-
-		TextView dispoParkRelaiText = (TextView) v.findViewById(R.id.dispoparkrelai_text);
-		dispoParkRelaiText.setText(parkRelai.getCarParkAvailable() + " / " + parkRelai.getCarParkCapacity());
 		TextView dispoParkRelaiStation = (TextView) v.findViewById(R.id.dispoparkrelai_nom);
 		dispoParkRelaiStation.setText(parkRelai.getName());
 		TextView dispoParkRelaiDistance = (TextView) v.findViewById(R.id.dispoparkrelai_distance);
 		dispoParkRelaiDistance.setText(parkRelai.formatDistance());
+		TextView dispoParkRelaiText = (TextView) v.findViewById(R.id.dispoparkrelai_text);
+		ImageView icone = (ImageView) v.findViewById(R.id.dispoparkrelai_image);
+		// Parc Relai ouvert.
+		if (parkRelai.getState().intValue() == 0) {
+			double poucentageDispo = ((double) parkRelai.getCarParkAvailable().intValue()) / ((double) (parkRelai.getCarParkCapacity().intValue()));
+
+
+			if (poucentageDispo < SEUIL_ROUGE) {
+				icone.setImageResource(R.drawable.dispo_parkrelai_rouge);
+			} else if (poucentageDispo < SEUIL_ORANGE) {
+				icone.setImageResource(R.drawable.dispo_parkrelai_orange);
+			} else {
+				icone.setImageResource(R.drawable.dispo_parkrelai_bleue);
+			}
+
+			dispoParkRelaiText.setText(parkRelai.getCarParkAvailable() + " / " + parkRelai.getCarParkCapacity());
+		} else {
+			// Cas ou le park relai n'est pas disponible (complet, fermé, indisponible).
+			icone.setImageResource(R.drawable.dispo_parkrelai_rouge);
+			dispoParkRelaiText.setText(MAP_STATES.get(parkRelai.getState()));
+		}
 		return v;
 	}
 }
