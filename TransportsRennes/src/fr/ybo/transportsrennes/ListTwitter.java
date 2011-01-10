c6dedb124a23efdee1b4cee1c67da94a2831355b
ybonnel <ybonnel@gmail.com>
2011-01-10 22:02:03 +0100

    Amélioration du message d'erreur lors du dépassement de quota pour twitter.

--- TransportsRennes/src/fr/ybo/transportsrennes/ListTwitter.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/ListTwitter.java
@@ -13,11 +13,23 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ListTwitter extends MenuAccueil.ListActivity {
+
+	private static TwitterFactory twitterFactory = null;
+
+	synchronized protected TwitterFactory getFactory() {
+		if (twitterFactory == null) {
+			twitterFactory = new TwitterFactory();
+		}
+		return twitterFactory;
+	}
+
+	private static final SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy à hh:mm");
 
 	private static final LogYbo LOG_YBO = new LogYbo(ListTwitter.class);
 
@@ -37,13 +49,19 @@ public class ListTwitter extends MenuAccueil.ListActivity {
 		new AsyncTask<Void, Void, Void>() {
 
 			private boolean erreur = false;
+			private boolean rateLimit = false;
+			private String dateReset = null;
 
 			@Override
 			protected Void doInBackground(final Void... pParams) {
-				Twitter twitter = new TwitterFactory().getInstance();
+				Twitter twitter = getFactory().getInstance();
 				try {
 					allStatus.addAll(twitter.getUserTimeline("@starbusmetro"));
 				} catch (TwitterException e) {
+					if (e.exceededRateLimitation()) {
+						rateLimit = true;
+						dateReset = SDF.format(e.getRateLimitStatus().getResetTime());
+					}
 					LOG_YBO.erreur("Erreur lors de la récupération des status twitter", e);
 					erreur = true;
 				}
@@ -56,7 +74,11 @@ public class ListTwitter extends MenuAccueil.ListActivity {
 				super.onPostExecute(pResult);
 				((TwitterAdapter) getListAdapter()).notifyDataSetChanged();
 				myProgressDialog.dismiss();
-				if (erreur) {
+				if (rateLimit) {
+					Toast.makeText(ListTwitter.this,
+							"Le nombre de demande à twitter pour votre IP a été dépassé (150/h). Cette limitation sera remise à zero le " + dateReset,
+							Toast.LENGTH_LONG).show();
+				} else if (erreur) {
 					Toast.makeText(ListTwitter.this, "Erreur lors de la récupération des messages twitter.", Toast.LENGTH_LONG).show();
 					ListTwitter.this.finish();
 				}
