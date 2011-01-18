54dd8de9c3c57bc4693eb4b2e32d699ff6d9bc19
ybonnel <ybonnel@gmail.com>
2011-01-19 00:37:15 +0100

    Désactive les logs debug.

--- TransportsRennes/src/fr/ybo/transportsrennes/util/LogYbo.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/util/LogYbo.java
@@ -21,13 +21,16 @@ public class LogYbo {
 
 	private static final String PREFIX_TAG = "YBO_";
 	private final String tag;
+	private static final boolean isDebug = false;
 
 	public LogYbo(final Class<?> clazz) {
 		tag = PREFIX_TAG + clazz.getSimpleName();
 	}
 
 	public void debug(final String message) {
-		Log.d(tag, message);
+		if (isDebug) {
+			Log.d(tag, message);
+		}
 	}
 
 	public void erreur(final String message, final Throwable throwable) {
