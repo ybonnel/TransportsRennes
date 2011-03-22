2dde972feb66d9f9d4688cead1482fc65fe0a536
ybonnel <ybonnel@gmail.com>
2011-03-22 22:45:00 +0100

    Suppression d'un warning

--- GeoCoder/src/com/google/code/geocoder/model/GeoAddress.java
+++ GeoCoder/src/com/google/code/geocoder/model/GeoAddress.java
@@ -17,7 +17,7 @@ package com.google.code.geocoder.model;
 class GeoAddress {
 	protected GeocoderLocationType locationType;
 	protected String formattedAddress;
-	private AddressDetails addressDetails;
+	protected AddressDetails addressDetails;
 	protected LatLng location;
 	protected LatLngBounds viewport;
 }
