77e42a7e6bfe1b6b5d75ddf705d70ac1cf5cc13f
ybonnel <ybonnel@gmail.com>
2011-04-26 17:13:01 +0200

    Gestion des directions.

--- TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/modeletcb/Ligne.java
+++ TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/modeletcb/Ligne.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import fr.ybo.transportsbordeauxhelper.modeletcb.sax.GetDirectionsHandler;
 import fr.ybo.transportsbordeauxhelper.modeletcb.sax.GetLignesHandler;
 
 public class Ligne {
@@ -17,7 +18,35 @@ public class Ligne {
 	public String identifiant;
 	public String type;
 	public String nom;
+	public String directionForward;
+	public String directionBackward;
 	
+	protected void remplirDirections() {
+
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(TcbConstantes.TCB_DIRECTION.replaceAll("\\{stop_id\\}", identifiant)).openConnection();
+			connection.setRequestMethod("GET");
+			connection.setDoOutput(true);
+			connection.connect();
+			BufferedReader bufReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			StringBuilder stringBuilder = new StringBuilder();
+			String ligne = bufReader.readLine();
+			while (ligne != null) {
+				stringBuilder.append(ligne.replaceAll("id=\"\"", ""));
+				ligne = bufReader.readLine();
+			}
+			
+			GetDirectionsHandler handler = new GetDirectionsHandler();
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser parser = factory.newSAXParser();
+			parser.parse(new ByteArrayInputStream(stringBuilder.toString().getBytes()), handler);
+			directionBackward = handler.getDirectionBackward();
+			directionForward = handler.getDirectionForward();
+			
+		} catch (Exception exception) {
+			throw new TcbException(exception);
+		}
+	}
 	
 	
 	public static List<Ligne> getLignes() {
@@ -28,18 +57,21 @@ public class Ligne {
 			connection.connect();
 			BufferedReader bufReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			try {
-				String ligne = bufReader.readLine();
-				while (ligne != null) {
-					System.out.println(ligne);
-					if (ligne.contains(TcbConstantes.BLOC_LIGNES_SELECT_NAME)) {
-						ligne = ligne.replaceAll("id=\"edit-navitia-line\" ", "");
+				String chaine = bufReader.readLine();
+				while (chaine != null) {
+					if (chaine.contains(TcbConstantes.BLOC_LIGNES_SELECT_NAME)) {
+						chaine = chaine.replaceAll("id=\"edit-navitia-line\" ", "");
 						GetLignesHandler handler = new GetLignesHandler();
 						SAXParserFactory factory = SAXParserFactory.newInstance();
 						SAXParser parser = factory.newSAXParser();
-						parser.parse(new ByteArrayInputStream(ligne.getBytes()), handler);
+						parser.parse(new ByteArrayInputStream(chaine.getBytes()), handler);
+						for (Ligne ligne : handler.getLignes()) {
+							System.out.println("Ajout des directions de " + ligne.nom);
+							ligne.remplirDirections();
+						}
 						return handler.getLignes();
 					}
-					ligne = bufReader.readLine();
+					chaine = bufReader.readLine();
 				}
 				return null;
 			} finally {
@@ -51,16 +83,16 @@ public class Ligne {
 	}
 
 
-
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "Ligne [identifiant=" + this.identifiant + ", type=" + this.type
-				+ ", nom=" + this.nom + "]";
-	}
-
+				+ ", nom=" + this.nom + ", directionForward="
+				+ this.directionForward + ", directionBackward="
+				+ this.directionBackward + "]";
+	}	
 	
 	
 }
--- TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/modeletcb/TcbConstantes.java
+++ TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/modeletcb/TcbConstantes.java
@@ -5,5 +5,6 @@ public class TcbConstantes {
 	public static final String TCB_URL = "http://www.infotbc.com/?nomobile=true";
 	
 	public static final String BLOC_LIGNES_SELECT_NAME = "navitia_line";
+	public static final String TCB_DIRECTION = "http://www.infotbc.com/ahah/stoppoint?navitia_line={stop_id}";
 
 }
