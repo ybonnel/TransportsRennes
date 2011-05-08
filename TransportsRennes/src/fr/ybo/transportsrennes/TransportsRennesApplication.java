86f6afeebdadeab0419dfdd5aba0165685a5f87c
ybonnel <ybonnel@gmail.com>
2011-05-08 20:57:21 +0200

    Ajout d'un système de notification de mise à jour.

--- TransportsRennes/src/fr/ybo/transportsrennes/TransportsRennesApplication.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/TransportsRennesApplication.java
@@ -19,9 +19,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
@@ -37,6 +41,7 @@ import fr.ybo.transportsrennes.keolis.gtfs.database.DataBaseHelper;
 import fr.ybo.transportsrennes.keolis.modele.bus.Alert;
 import fr.ybo.transportsrennes.util.CalculItineraires;
 import fr.ybo.transportsrennes.util.Constantes;
+import fr.ybo.transportsrennes.util.Version;
 
 /**
  * Classe de l'application permettant de stocker les attributs globaux à l'application.
@@ -63,12 +68,7 @@ public class TransportsRennesApplication extends Application {
 		handler = new Handler();
 		myTraker = new TransportsRennesApplication.MyTraker(traker);
 		myTraker.trackPageView("/TransportsRennesApplication/Model/" + Build.MODEL);
-		PackageManager manager = getPackageManager();
-		try {
-			PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
-			myTraker.trackPageView("/TransportsRennesApplication/Version/" + info.versionName);
-		} catch (PackageManager.NameNotFoundException ignore) {
-		}
+		myTraker.trackPageView("/TransportsRennesApplication/Version/" + Version.getVersionCourante(this));
 
 		// Récupération des alertes
 		new AsyncTask<Void, Void, Void>(){
@@ -88,6 +88,8 @@ public class TransportsRennesApplication extends Application {
 				return null;
 			}
 		}.execute();
+
+		checkVersion.execute();
 	}
 
 	private static Handler handler;
@@ -127,6 +129,43 @@ public class TransportsRennesApplication extends Application {
 
 	public static LatLngBounds getBounds() {
 		return bounds;
+	}
+
+	private AsyncTask<Void, Void, String> checkVersion = new AsyncTask<Void, Void, String>() {
+
+		@Override
+		protected String doInBackground(Void... params) {
+			return Version.getVersionMarket();
+		}
+
+		protected void onPostExecute(String result) {
+			if (result != null && !result.equals(Version.getVersionCourante(TransportsRennesApplication.this))) {
+				createNotification(result);
+			}
+		};
+	};
+
+	private final int NOTIFICATION_VERSION_ID = 1;
+
+	private void createNotification(String nouvelleVersion) {
+		int icon = R.drawable.icon;
+		CharSequence tickerText = getString(R.string.nouvelleVersion);
+		long when = System.currentTimeMillis();
+		Context context = getApplicationContext();
+		CharSequence contentTitle = getString(R.string.nouvelleVersion);
+		CharSequence contentText = getString(R.string.versionDisponible, nouvelleVersion);
+
+		Uri uri = Uri.parse("market://details?id=fr.ybo.transportsrennes");
+		Intent notificationIntent = new Intent(Intent.ACTION_VIEW, uri);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+		// the next two lines initialize the Notification, using the
+		// configurations above
+		Notification notification = new Notification(icon, tickerText, when);
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.notify(NOTIFICATION_VERSION_ID, notification);
 	}
 
 }
