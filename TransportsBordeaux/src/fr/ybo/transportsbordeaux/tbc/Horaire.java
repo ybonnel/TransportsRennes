7ba38383d171befdcc5e20ceb61f6685c0b65692
ybonnel <ybonnel@gmail.com>
2011-05-16 10:57:06 +0200

    Ajout d'infos dans l'exception lors d'une erreur de récupération des horaires.

--- TransportsBordeaux/src/fr/ybo/transportsbordeaux/tbc/Horaire.java
+++ TransportsBordeaux/src/fr/ybo/transportsbordeaux/tbc/Horaire.java
@@ -26,11 +26,11 @@ public class Horaire implements Serializable {
 	private static final LogYbo LOG_YBO = new LogYbo(Horaire.class);
 
 	public static List<Horaire> getHoraires(Date date, ArretFavori favori) {
+		String url = TcbConstantes.getUrlHoraire(favori.ligneId, favori.arretId,
+				favori.macroDirection.intValue() == 0, date);
 		try {
 			// Récupération sur la page internet du table d'horaire.
 			StringBuilder stringBuilder = new StringBuilder();
-			String url = TcbConstantes.getUrlHoraire(favori.ligneId, favori.arretId,
-					favori.macroDirection.intValue() == 0, date);
 			LOG_YBO.debug(url);
 			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
 			connection.setRequestMethod("GET");
@@ -65,7 +65,7 @@ public class Horaire implements Serializable {
 			parser.parse(new ByteArrayInputStream(stringBuilder.toString().getBytes()), handler);
 			return handler.getHoraires();
 		} catch (Exception exception) {
-			throw new TcbException(exception);
+			throw new TcbException("Erreur lors de la récupération des horaires pour l'url " + url, exception);
 		}
 	}
 
