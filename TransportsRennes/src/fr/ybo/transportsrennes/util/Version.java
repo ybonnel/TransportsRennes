44b60b8478566656dd03a15315e9b56712241636
ybonnel <ybonnel@gmail.com>
2011-09-29 22:49:42 +0200

    On ignore les erreurs lors de la fermeture du Reader.

--- TransportsRennes/src/fr/ybo/transportsrennes/util/Version.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/util/Version.java
@@ -17,7 +17,6 @@
 package fr.ybo.transportsrennes.util;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
@@ -85,7 +84,7 @@ public class Version {
 		if (reader != null) {
 			try {
 				reader.close();
-			} catch (IOException ignore) {
+			} catch (Exception ignore) {
 			}
 		}
 	}
