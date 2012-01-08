4d9e2891dfc16cf072ac986fb476ec07aa905cf5
ybonnel <ybonnel@gmail.com>
2012-01-08 22:31:47 +0100

    Onglets "ICS" like pour les alerts (#68).

---
+++ TransportsRennes/src/fr/ybo/transportsrennes/activity/alerts/ListAlertsForOneLine.java
@@ -0,0 +1,36 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.ybo.transportsrennes.activity.alerts;
+
+import android.os.Bundle;
+import fr.ybo.transportsrennes.R;
+import fr.ybo.transportsrennes.activity.commun.BaseActivity.BaseFragmentActivity;
+import fr.ybo.transportsrennes.database.modele.Ligne;
+import fr.ybo.transportsrennes.fragments.alerts.ListAlerts;
+
+public class ListAlertsForOneLine extends BaseFragmentActivity {
+
+    private Ligne ligne;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ligne = (Ligne) getIntent().getExtras().getSerializable("ligne");
+		setContentView(R.layout.listalert);
+		getActivityHelper().setupActionBar(R.menu.default_menu_items);
+		ListAlerts fragmentAlert = (ListAlerts) getSupportFragmentManager().findFragmentById(R.id.fragment_alerts);
+		fragmentAlert.setLigne(ligne);
+    }
+
+}
--- TransportsRennes/src/fr/ybo/transportsrennes/activity/alerts/TabAlertes.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/activity/alerts/TabAlertes.java
@@ -13,44 +13,169 @@
  */
 package fr.ybo.transportsrennes.activity.alerts;
 
+import java.util.ArrayList;
 
-import android.content.Intent;
-import android.content.res.Resources;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TabHost;
+import android.widget.TabWidget;
 import fr.ybo.transportsrennes.R;
-import fr.ybo.transportsrennes.activity.commun.BaseActivity.BaseTabActivity;
+import fr.ybo.transportsrennes.activity.commun.BaseActivity.BaseFragmentActivity;
 import fr.ybo.transportsrennes.application.TransportsRennesApplication;
 
-public class TabAlertes extends BaseTabActivity {
+public class TabAlertes extends BaseFragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	TabHost mTabHost;
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 		TransportsRennesApplication.majTheme(this);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tabalertes);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabalertes);
 		getActivityHelper().setupActionBar(R.menu.default_menu_items);
 
+		mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+		mTabHost.setup();
 
-        Resources res = getResources();
-        TabHost tabHost = getTabHost();
+		mViewPager = (ViewPager) findViewById(R.id.pager);
 
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        Intent intent = new Intent().setClass(this, ListAlerts.class);
+		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
 
-        // Initialize a TabSpec for each tab and add it to the TabHost
-        TabHost.TabSpec spec =
-                tabHost.newTabSpec("alertes").setIndicator(getString(R.string.alertes), res.getDrawable(android.R.drawable.ic_dialog_alert))
-                        .setContent(intent);
-        tabHost.addTab(spec);
+		mTabsAdapter.addTab(
+				mTabHost.newTabSpec("alertes").setIndicator(getString(R.string.alertes),
+						getResources().getDrawable(android.R.drawable.ic_dialog_alert)),
+				fr.ybo.transportsrennes.fragments.alerts.ListAlerts.class, null);
 
-        // Do the same for the other tabs
-        Intent intentTwitter = new Intent().setClass(this, ListTwitter.class);
-        TabHost.TabSpec twitterSpec =
-                tabHost.newTabSpec("twitter").setIndicator(getString(R.string.twitter), res.getDrawable(R.drawable.ic_menu_twitter))
-                        .setContent(intentTwitter);
-        tabHost.addTab(twitterSpec);
+		mTabsAdapter.addTab(
+				mTabHost.newTabSpec("twitter").setIndicator(getString(R.string.twitter),
+						getResources().getDrawable(R.drawable.ic_menu_twitter)),
+				fr.ybo.transportsrennes.fragments.alerts.ListTwitter.class, null);
 
-        tabHost.setCurrentTab(0);
-    }
+		if (savedInstanceState != null) {
+			mTabHost.setCurrentTabByTag(savedInstanceState.getString("tab"));
+		} else {
+			mTabHost.setCurrentTab(0);
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString("tab", mTabHost.getCurrentTabTag());
+	}
+
+	/**
+	 * This is a helper class that implements the management of tabs and all
+	 * details of connecting a ViewPager with associated TabHost. It relies on a
+	 * trick. Normally a tab host has a simple API for supplying a View or
+	 * Intent that each tab will show. This is not sufficient for switching
+	 * between pages. So instead we make the content part of the tab host 0dp
+	 * high (it is not shown) and the TabsAdapter supplies its own dummy view to
+	 * show as the tab content. It listens to changes in tabs, and takes care of
+	 * switch to the correct paged in the ViewPager whenever the selected tab
+	 * changes.
+	 */
+	public static class TabsAdapter extends FragmentPagerAdapter implements TabHost.OnTabChangeListener,
+			ViewPager.OnPageChangeListener {
+		private final Context mContext;
+		private final TabHost mTabHost;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+		static final class TabInfo {
+			private final String tag;
+			private final Class<?> clss;
+			private final Bundle args;
+
+			TabInfo(String _tag, Class<?> _class, Bundle _args) {
+				tag = _tag;
+				clss = _class;
+				args = _args;
+			}
+		}
+
+		static class DummyTabFactory implements TabHost.TabContentFactory {
+			private final Context mContext;
+
+			public DummyTabFactory(Context context) {
+				mContext = context;
+			}
+
+			@Override
+			public View createTabContent(String tag) {
+				View v = new View(mContext);
+				v.setMinimumWidth(0);
+				v.setMinimumHeight(0);
+				return v;
+			}
+		}
+
+		public TabsAdapter(FragmentActivity activity, TabHost tabHost, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mTabHost = tabHost;
+			mViewPager = pager;
+			mTabHost.setOnTabChangedListener(this);
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+
+		public void addTab(TabHost.TabSpec tabSpec, Class<?> clss, Bundle args) {
+			tabSpec.setContent(new DummyTabFactory(mContext));
+			String tag = tabSpec.getTag();
+
+			TabInfo info = new TabInfo(tag, clss, args);
+			mTabs.add(info);
+			mTabHost.addTab(tabSpec);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+		}
+
+		@Override
+		public void onTabChanged(String tabId) {
+			int position = mTabHost.getCurrentTab();
+			mViewPager.setCurrentItem(position);
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			// Unfortunately when TabHost changes the current tab, it kindly
+			// also takes care of putting focus on it when not in touch mode.
+			// The jerk.
+			// This hack tries to prevent this from pulling focus out of our
+			// ViewPager.
+			TabWidget widget = mTabHost.getTabWidget();
+			int oldFocusability = widget.getDescendantFocusability();
+			widget.setDescendantFocusability(ViewGroup.FOCUS_BLOCK_DESCENDANTS);
+			mTabHost.setCurrentTab(position);
+			widget.setDescendantFocusability(oldFocusability);
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+		}
+	}
+
 }
