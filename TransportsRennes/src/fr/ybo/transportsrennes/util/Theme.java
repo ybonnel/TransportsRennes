4a8c5574c8fda24049114c20228a199d86208e10
ybonnel <ybonnel@gmail.com>
2012-01-17 22:16:33 +0100

    ActionBar noir pour le thème noir. Fixed #91

--- TransportsRennes/src/fr/ybo/transportsrennes/util/Theme.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/util/Theme.java
@@ -4,14 +4,17 @@ import android.graphics.Color;
 import fr.ybo.transportsrennes.R;
 
 public enum Theme {
-	NOIR(Color.LTGRAY, R.style.Theme_TransportsRennes_black), BLANC(Color.BLACK, R.style.Theme_TransportsRennes);
+	NOIR(Color.LTGRAY, R.style.Theme_TransportsRennes_black, R.drawable.actionbar_compat_background_black), BLANC(
+			Color.BLACK, R.style.Theme_TransportsRennes, R.drawable.actionbar_compat_background);
 
 	private int textColor;
 	private int theme;
+	private int actionBarBackground;
 
-	private Theme(int textColor, int theme) {
+	private Theme(int textColor, int theme, int actionBarBackground) {
 		this.textColor = textColor;
 		this.theme = theme;
+		this.actionBarBackground = actionBarBackground;
 	}
 
 	public int getTextColor() {
@@ -20,6 +23,10 @@ public enum Theme {
 
 	public int getTheme() {
 		return theme;
+	}
+
+	public int getActionBarBackground() {
+		return actionBarBackground;
 	}
 
 }
