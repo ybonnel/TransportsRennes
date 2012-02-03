89757391bf34545bb76a20d6b48554dbfb3e553f
ybonnel <ybonnel@gmail.com>
2012-02-03 16:41:42 +0100

    Meilleur gestion de la migration des données. #96.

--- TransportsBordeaux/src/fr/ybo/transportsbordeaux/database/TransportsBordeauxDatabase.java
+++ TransportsBordeaux/src/fr/ybo/transportsbordeaux/database/TransportsBordeauxDatabase.java
@@ -13,6 +13,7 @@
  */
 package fr.ybo.transportsbordeaux.database;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -40,7 +41,7 @@ import fr.ybo.transportscommun.donnees.modele.VeloFavori;
 public class TransportsBordeauxDatabase extends DataBaseHelper {
 
     public static final String DATABASE_NAME = "transportsbordeaux.db";
-	private static final int DATABASE_VERSION = 9;
+	private static final int DATABASE_VERSION = 8;
 
     @SuppressWarnings("unchecked")
     private static final List<Class<?>> DATABASE_ENTITITES =
@@ -117,16 +118,50 @@ public class TransportsBordeauxDatabase extends DataBaseHelper {
             });
 			mapUpgrades.put(8, new UpgradeDatabase() {
 				public void upgrade(SQLiteDatabase db) {
-					getBase().getTable(VeloFavori.class).dropTable(db);
-					getBase().getTable(VeloFavori.class).createTable(db);
-				}
-			});
-			mapUpgrades.put(9, new UpgradeDatabase() {
-				public void upgrade(SQLiteDatabase db) {
-					db.execSQL("ALTER TABLE Trajet ADD COLUMN macroDirection INTEGER");
-					db.execSQL("ALTER TABLE ArretRoute ADD COLUMN macroDirection INTEGER");
-					db.execSQL("ALTER TABLE ArretFavori ADD COLUMN macroDirection INTEGER");
-					db.execSQL("UPDATE ArretFavori SET macroDirection = 0");
+					getBase().getTable(Trajet.class).dropTable(db);
+					getBase().getTable(ArretRoute.class).dropTable(db);
+					getBase().getTable(DernierMiseAJour.class).dropTable(db);
+					getBase().getTable(Trajet.class).createTable(db);
+					getBase().getTable(ArretRoute.class).createTable(db);
+					getBase().getTable(DernierMiseAJour.class).createTable(db);
+
+					db.execSQL("ALTER TABLE ArretFavori RENAME TO ArretFavori_tmp");
+					getBase().getTable(ArretFavori.class).createTable(db);
+					List<String> columns = new ArrayList<String>(7);
+					columns.add("arretId");
+					columns.add("ligneId");
+					columns.add("nomArret");
+					columns.add("direction");
+					columns.add("nomCourt");
+					columns.add("nomLong");
+					columns.add("ordre");
+					columns.add("groupe");
+					Cursor arretFavoriTmp = db.query("ArretFavori_tmp", columns.toArray(new String[7]), null, null,
+							null, null, null);
+					int arretIdIndex = arretFavoriTmp.getColumnIndex("arretId");
+					int ligneIdIndex = arretFavoriTmp.getColumnIndex("ligneId");
+					int nomArretIndex = arretFavoriTmp.getColumnIndex("nomArret");
+					int directionIndex = arretFavoriTmp.getColumnIndex("direction");
+					int nomCourtIndex = arretFavoriTmp.getColumnIndex("nomCourt");
+					int nomLongIndex = arretFavoriTmp.getColumnIndex("nomLong");
+					int ordreIndex = arretFavoriTmp.getColumnIndex("ordre");
+					int groupeIndex = arretFavoriTmp.getColumnIndex("groupe");
+					ArretFavori favori = new ArretFavori();
+					int count = 1;
+					while (arretFavoriTmp.moveToNext()) {
+						favori.arretId = arretFavoriTmp.getString(arretIdIndex);
+						favori.ligneId = arretFavoriTmp.getString(ligneIdIndex);
+						favori.nomArret = arretFavoriTmp.getString(nomArretIndex);
+						favori.direction = arretFavoriTmp.getString(directionIndex);
+						favori.nomCourt = arretFavoriTmp.getString(nomCourtIndex);
+						favori.nomLong = arretFavoriTmp.getString(nomLongIndex);
+						favori.ordre = arretFavoriTmp.isNull(ordreIndex) ? count : arretFavoriTmp.getInt(ordreIndex);
+						favori.groupe = arretFavoriTmp.getString(groupeIndex);
+						favori.macroDirection = 0;
+						count++;
+						getBase().insert(db, favori);
+					}
+					db.execSQL("DROP TABLE ArretFavori_tmp");
 				}
 			});
         }
