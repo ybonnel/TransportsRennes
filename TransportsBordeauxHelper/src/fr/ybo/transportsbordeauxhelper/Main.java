9a2e6cac6e7a64e3416fd54d016d5fec70544207
ybonnel <ybonnel@gmail.com>
2012-05-02 22:38:35 +0200

    Prise en compte des nouveaux horaires pour Bordeaux.

--- TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
@@ -72,7 +72,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(false, "20120401");
+		genereGtfs(false, "20120501");
     }
 
     /**
