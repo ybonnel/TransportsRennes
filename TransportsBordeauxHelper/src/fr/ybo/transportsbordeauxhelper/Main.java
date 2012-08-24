de5e0f4058362a9c18d3312f2c67408272d76c85
ybonnel <ybonnel@gmail.com>
2012-08-24 17:51:08 +0200

    Horaires de rentrée pour Bordeaux

--- TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
@@ -72,7 +72,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(false, "20120822");
+		genereGtfs(false, "20120827");
     }
 
     /**
