6c9a04888a1b5f7a69ffe15011f938a677119c1d
ybonnel <ybonnel@gmail.com>
2012-12-20 13:14:02 +0100

    Gestion des mises à jour d'horaires avec le racourci favoris

--- TransportsRennes/src/fr/ybo/transportsrennes/activity/bus/TabFavoris.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/activity/bus/TabFavoris.java
@@ -13,15 +13,82 @@
  */
 package fr.ybo.transportsrennes.activity.bus;
 
+import java.util.Date;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import fr.ybo.database.DataBaseException;
+import fr.ybo.database.DataBaseHelper;
 import fr.ybo.transportscommun.activity.bus.AbstractTabFavoris;
 import fr.ybo.transportscommun.activity.commun.BaseActivity.BaseFragmentActivity;
+import fr.ybo.transportscommun.donnees.manager.gtfs.GestionZipKeolis;
+import fr.ybo.transportscommun.donnees.modele.DernierMiseAJour;
 import fr.ybo.transportsrennes.R;
 import fr.ybo.transportsrennes.activity.loading.LoadingActivity;
+import fr.ybo.transportsrennes.application.TransportsRennesApplication;
 import fr.ybo.transportsrennes.fragments.bus.ListFavoris;
 
 public class TabFavoris extends AbstractTabFavoris {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		verifierUpgrade();
+	}
+	
+
+	private void verifierUpgrade() {
+		DataBaseHelper dataBaseHelper = TransportsRennesApplication.getDataBaseHelper();
+		DernierMiseAJour dernierMiseAJour = null;
+		try {
+			dernierMiseAJour = dataBaseHelper.selectSingle(new DernierMiseAJour());
+		} catch (DataBaseException exception) {
+			dataBaseHelper.deleteAll(DernierMiseAJour.class);
+		}
+		Date dateDernierFichierKeolis = GestionZipKeolis.getLastUpdate(getResources(), R.raw.last_update);
+		if (dernierMiseAJour == null) {
+			upgradeDatabase();
+		} else if (dernierMiseAJour.derniereMiseAJour == null
+				|| dateDernierFichierKeolis.after(dernierMiseAJour.derniereMiseAJour)) {
+			showDialog(DIALOG_UPGRADE);
+		}
+	}
+
+
+	private void upgradeDatabase() {
+		Intent intent = new Intent(this, LoadingActivity.class);
+		intent.putExtra("operation", LoadingActivity.OPERATION_UPGRADE_DATABASE);
+		startActivity(intent);
+	}
+	
+	private static final int DIALOG_UPGRADE = 2;
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		if (id == DIALOG_UPGRADE) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(getString(R.string.majDispo));
+			builder.setCancelable(false);
+			builder.setPositiveButton(getString(R.string.oui), new Dialog.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.dismiss();
+					upgradeDatabase();
+				}
+			});
+			builder.setNegativeButton(getString(R.string.non), new Dialog.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
+			return builder.create();
+		}
+		return super.onCreateDialog(id);
+	}
+	
 
 	@Override
 	protected int getLayout() {
