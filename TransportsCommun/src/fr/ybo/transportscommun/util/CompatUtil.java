f8880cab83a8796378d3083cab7f8b5233a386bd
ybonnel <ybonnel@gmail.com>
2012-12-21 18:55:21 +0100

    Lint

--- TransportsCommun/src/fr/ybo/transportscommun/util/CompatUtil.java
+++ TransportsCommun/src/fr/ybo/transportscommun/util/CompatUtil.java
@@ -1,9 +1,12 @@
 package fr.ybo.transportscommun.util;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 
 public class CompatUtil {
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	public static void invalidateOptionsMenu(Activity activity) {
 		activity.invalidateOptionsMenu();
 	}
