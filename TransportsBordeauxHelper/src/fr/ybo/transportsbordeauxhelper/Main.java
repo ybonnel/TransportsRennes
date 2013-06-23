c2d2afb77b491a63ee2be2aaf2be80880f6c5b38
ybonnel <ybonnel@gmail.com>
2013-06-23 19:10:41 +0200

    Mise a jour des horaires de Transports Bordeaux

--- TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/fr/ybo/transportsbordeauxhelper/Main.java
@@ -72,7 +72,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(true, "20130615");
+		genereGtfs(false, "20130622");
     }
 
     /**
