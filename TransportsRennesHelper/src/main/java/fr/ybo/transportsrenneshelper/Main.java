3d23a3989725830d3ed23a44dce2e9f4f67bd6a4
ybonnel <ybonnel@gmail.com>
2014-05-11 17:06:49 +0200

    Nouveaux horaires de Rennes

--- TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
+++ TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
@@ -63,7 +63,7 @@ public final class Main {
 	 *             problème d'entrée/sortie.
 	 */
 	public static void main(String[] args) throws IOException, CsvErrorsExceededException {
-		genereGtfs(false, OPTIMIZE_CALENDARS, "20140407");
+		genereGtfs(false, OPTIMIZE_CALENDARS, "20140430");
 		// genereParcoursBus("20120912");
 	}
 
