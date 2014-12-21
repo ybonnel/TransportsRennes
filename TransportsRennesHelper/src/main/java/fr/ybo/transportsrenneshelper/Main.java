67fdd6a353fd25266c352094201443c5d980f151
ybonnel <ybonnel@gmail.com>
2014-12-21 18:11:13 +0100

    Mise a jour des horaires de Rennes

--- TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
+++ TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
@@ -63,7 +63,7 @@ public final class Main {
 	 *             problème d'entrée/sortie.
 	 */
 	public static void main(String[] args) throws IOException, CsvErrorsExceededException {
-		genereGtfs(true, OPTIMIZE_CALENDARS, "20141024");
+		genereGtfs(true, OPTIMIZE_CALENDARS, "20141212");
 		// genereParcoursBus("20120912");
 	}
 
