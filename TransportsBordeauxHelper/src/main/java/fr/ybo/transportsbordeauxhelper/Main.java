a7d9dd73266f75a320208e132002af3ba9dd86ef
ybonnel <ybonnel@gmail.com>
2015-06-28 17:31:26 +0200

    Maj Rennes et Bordeaux

--- TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
@@ -73,7 +73,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(true, "20150620");
+		genereGtfs(false, "20150627");
     }
 
     /**
--- TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
+++ TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
@@ -63,7 +63,7 @@ public final class Main {
 	 *             problème d'entrée/sortie.
 	 */
 	public static void main(String[] args) throws IOException, CsvErrorsExceededException {
-		genereGtfs(false, OPTIMIZE_CALENDARS, "20150617");
+		genereGtfs(false, OPTIMIZE_CALENDARS, "20150623");
 		// genereParcoursBus("20120912");
 	}
 
