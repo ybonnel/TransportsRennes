2d30e8f7a43186197d5527a815b3d556d7ee8aed
ybonnel <ybonnel@gmail.com>
2015-08-30 14:02:45 +0200

    Mise a jour bordeaux et rennes

--- TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
@@ -73,7 +73,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(true, "20150726");
+		genereGtfs(true, "20150830");
     }
 
     /**
