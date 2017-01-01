ad43c1682eea94a3cbf1b44c74bf07501c37146e
ybonnel <ybonnel@gmail.com>
2017-01-01 19:53:08 +0100

    Maj Rennes et Bdx

--- TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
+++ TransportsBordeauxHelper/src/main/java/fr/ybo/transportsbordeauxhelper/Main.java
@@ -73,7 +73,7 @@ public final class Main {
      * @throws ParseException
      */
     public static void main(String[] args) throws IOException, ParseException {
-		genereGtfs(true, "20161212");
+		genereGtfs(false, "20170101");
     }
 
     /**
