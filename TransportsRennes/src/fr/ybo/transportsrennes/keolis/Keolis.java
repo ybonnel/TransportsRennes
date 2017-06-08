4fcbff484f4c5d3a51e30e610c3f2910413c6f55
Yan Bonnel <ybonnel@gmail.com>
2017-06-08 21:54:47 +0200

    Maj Rennes et Bdx

--- TransportsRennes/src/fr/ybo/transportsrennes/keolis/Keolis.java
+++ TransportsRennes/src/fr/ybo/transportsrennes/keolis/Keolis.java
@@ -97,23 +97,26 @@ public final class Keolis {
 
     private <ObjetKeolis> List<ObjetKeolis> appelKeolis(KeolisHandler<ObjetKeolis> handler, String query)
             throws ErreurReseau {
-        LOG_YBO.debug("Appel d'une API Keolis sur le dataset '" + handler.getDatasetid() + '\'');
+        LOG_YBO.warn("Appel d'une API Keolis sur le dataset '" + handler.getDatasetid() + "' with query : " + query);
         long startTime = System.nanoTime() / 1000;
         HttpClient httpClient = HttpUtils.getHttpClient();
 
         String urlParams = "";
         if (query != null) {
             try {
-                urlParams = "&q=" + URLEncoder.encode(query, "UTF-8");
+                urlParams = "&where=" + URLEncoder.encode(query, "UTF-8");
             } catch (UnsupportedEncodingException ignore) {
             }
         }
 
+        String request = "http://data.explore.star.fr/api/v2/catalog/datasets/" +
+                handler.getDatasetid() +
+                "/records?rows=-1&pretty=false&timezone=CET&apikey=e519de4f9d490b95947ad21716127633e2b8445dd7cd18644e446958"
+                + urlParams;
+
+        LOG_YBO.warn(request);
         HttpUriRequest httpGet = new HttpGet(
-                "http://data.explore.star.fr/api/v2/catalog/datasets/" +
-                        handler.getDatasetid() +
-                        "/records?rows=-1&pretty=false&timezone=CET&apikey=e519de4f9d490b95947ad21716127633e2b8445dd7cd18644e446958"
-                        + urlParams
+                request
         );
         List<ObjetKeolis> result = new ArrayList<ObjetKeolis>();
         try {
@@ -221,11 +224,11 @@ public final class Keolis {
             return getDeparturesForMetro(favori);
         }
         return appelKeolis(new GetDeparturesHandler(),
-                "idarret:" + favori.arretId + " AND idligne:" + favori.ligneId + " AND sens:" + favori.macroDirection);
+                "idarret:\"" + favori.arretId + "\" AND idligne:\"" + favori.ligneId + "\" AND sens:" + favori.macroDirection);
     }
 
     public List<Departure> getDeparturesForMetro(ArretFavori favori) throws ErreurReseau {
-        return appelKeolis(new GetDeparturesMetroHandler(), "idarret:" + favori.arretId + " AND sens:" + favori.macroDirection);
+        return appelKeolis(new GetDeparturesMetroHandler(), "idarret:\"" + favori.arretId + "\" AND sens:" + favori.macroDirection);
     }
 
 
