3435f5eb5597f6fb3519d32777839b09f4b75ac2
Yan Bonnel <yan.bonnel@smart-rx.com>
2018-05-13 21:53:55 +0200

    Maj rennes et bdx

--- TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
+++ TransportsRennesHelper/src/main/java/fr/ybo/transportsrenneshelper/Main.java
@@ -63,7 +63,7 @@ public final class Main {
 	 *             problème d'entrée/sortie.
 	 */
 	public static void main(String[] args) throws IOException, CsvErrorsExceededException {
-		genereGtfs(false, OPTIMIZE_CALENDARS, "GTFS_2017.8.0.1_2018-04-23_2018-05-13");
+		genereGtfs(false, OPTIMIZE_CALENDARS, "GTFS_2017.9.0_2018-05-14_2018-06-10");
 		// genereParcoursBus("20120912");
 	}
 
